c7f8bf3866f5f0e168ff0e765c06145f2066b192
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-09-14 19:19:38 +0800

    Add project

---
+++ js/js.js
@@ -0,0 +1,29 @@
+// init skrollr
+$(function() {
+    skrollr.init();
+});
+
+// loading
+$(document).ready(function() {
+    //setTimeout(function() {
+    $('#loader-container').remove();
+    $('#content').css('visibility','visible');
+    $('body').css('overflow-y', 'visible');
+    //}, 5000);
+});
+
+// smooth scroll
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
+    });
+});
